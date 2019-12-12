dd036c8ca1219af4a923f27a2e84ff57e8a3803c
Mark Khazin <mark.khazin@intsights.com>
2019-12-12 17:12:54 +0200

    Added the Homoglyph Token Filter to the plugin

---
+++ src/main/java/com/intsights/elasticsearch/index/analysis/HomoglyphTokenFilterFactory.java
@@ -0,0 +1,19 @@
+package com.intsights.elasticsearch.index.analysis;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.index.IndexSettings;
+
+public class HomoglyphTokenFilterFactory extends AbstractTokenFilterFactory {
+
+    public HomoglyphTokenFilterFactory(IndexSettings indexSettings, Environment environment, String name, Settings settings) {
+        super(indexSettings, name, settings);
+    }
+
+    @Override
+    public TokenStream create(TokenStream tokenStream) {
+        return new HomoglyphTokenFilter(tokenStream);
+    }
+}
--- src/main/java/com/intsights/elasticsearch/plugin/analysis/AnalysisHomoglyphPlugin.java
+++ src/main/java/com/intsights/elasticsearch/plugin/analysis/AnalysisHomoglyphPlugin.java
@@ -1,10 +1,19 @@
 package com.intsights.elasticsearch.plugin.analysis;
 
+import com.intsights.elasticsearch.index.analysis.HomoglyphTokenFilterFactory;
+import org.elasticsearch.index.analysis.TokenFilterFactory;
+import org.elasticsearch.indices.analysis.AnalysisModule.AnalysisProvider;
 import org.elasticsearch.plugins.AnalysisPlugin;
 import org.elasticsearch.plugins.Plugin;
 
+import java.util.Map;
+
+import static java.util.Collections.singletonMap;
 
 public class AnalysisHomoglyphPlugin extends Plugin implements AnalysisPlugin {
-
+    @Override
+    public Map<String, AnalysisProvider<TokenFilterFactory>> getTokenFilters() {
+        return singletonMap("homoglyph", HomoglyphTokenFilterFactory::new);
+    }
 }
 
