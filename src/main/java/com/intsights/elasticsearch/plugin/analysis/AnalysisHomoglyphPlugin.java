8bee87a60ec752689f569537f0a6443619c5c082
Mark Khazin <mark.khazin@intsights.com>
2019-12-12 17:08:01 +0200

    Created an empty Elasticsearch plugin, ready to be built with Maven

---
+++ src/main/java/com/intsights/elasticsearch/plugin/analysis/AnalysisHomoglyphPlugin.java
@@ -0,0 +1,10 @@
+package com.intsights.elasticsearch.plugin.analysis;
+
+import org.elasticsearch.plugins.AnalysisPlugin;
+import org.elasticsearch.plugins.Plugin;
+
+
+public class AnalysisHomoglyphPlugin extends Plugin implements AnalysisPlugin {
+
+}
+
